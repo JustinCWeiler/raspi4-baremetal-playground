38378a07dd950a2bbeada4fa61f46ec3baa1c11e
Justin Weiler <jweiler@stanford.edu>
2022-03-19 10:22:48 -0700

    change blinky to use ACT led

--- simple/include/blinky.h
+++ simple/include/blinky.h
@@ -7,7 +7,9 @@
 #define GPIO_BASE (P_BASE + 0x200000)
 #define GPIO_FSEL2 (GPIO_BASE + 0x8)
 #define GPIO_SET0 (GPIO_BASE + 0x1c)
+#define GPIO_SET1 (GPIO_BASE + 0x20)
 #define GPIO_CLR0 (GPIO_BASE + 0x28)
+#define GPIO_CLR1 (GPIO_BASE + 0x2c)
 
 extern void wait(uint64_t time);
 extern void PUT32(uintptr_t addr, uint32_t val);
