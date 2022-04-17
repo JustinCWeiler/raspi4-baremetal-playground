21e2097d9d7f2e71ad0cc5814a9adb5665b4ae0f
Justin Weiler <jweiler@stanford.edu>
2022-04-17 16:47:38 -0700

    add bootdefs

---
+++ bootloader-unix/include/bootdefs.h
@@ -0,0 +1,13 @@
+#ifndef _BOOTDEFS_H_
+#define _BOOTDEFS_H_
+
+#define SUCCESS  0x00
+#define GET_INFO 0x11
+#define PUT_INFO 0x22
+#define GET_CODE 0x33
+#define PUT_CODE 0x44
+
+#define CRC_FAIL 0xF0
+#define TIMEOUT  0xF1
+
+#endif
