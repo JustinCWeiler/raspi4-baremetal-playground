6f59d9f322360ad941e7ce8021868894ed2928f2
Justin Weiler <jweiler@stanford.edu>
2022-05-04 23:27:51 -0700

    better simple test program

--- simple/src/simple.c
+++ simple/src/simple.c
@@ -3,8 +3,25 @@
 #include "uart.h"
 #include "gpio.h"
 
+#define AMOUNT 0x100000
+
+void send_msg(const char* msg) {
+	while (*msg) uart_write(*msg++);
+}
+
 void main(void) {
-	uart_init();
+	volatile int count;
+
+	for (int i = 0; i < 4; i++) {
+		gpio_act_on();
+		send_msg("gpio act on\n");
+		count = AMOUNT;
+		while (count--) ;
+		gpio_act_off();
+		send_msg("gpio act off\n");
+		count = AMOUNT;
+		while (count--) ;
+	}
 
 	return_to_bootloader();
 }
