2d8cc075ff9005241a73b0c96445a1332650f111
Justin Weiler <jweiler@stanford.edu>
2022-05-10 23:52:20 -0700

    make wait function

--- bootloader-c/src/main.c
+++ bootloader-c/src/main.c
@@ -6,6 +6,8 @@
 #define GPIO_SET1 ((volatile uint32_t*)(GPIO_BASE+0x20))
 #define GPIO_CLR1 ((volatile uint32_t*)(GPIO_BASE+0x2C))
 
+#define BLINK_WAIT 0xC0000
+
 static void gpio_act_on(void) {
 	// WRITE
 	*GPIO_SET1 = 1 << 10;
@@ -16,16 +18,18 @@ static void gpio_act_off(void) {
 	*GPIO_CLR1 = 1 << 10;
 }
 
+static void wait(volatile size_t w) {
+	while (w--) ;
+}
+
 void main(void) __attribute__((flatten, noreturn, nothrow));
 void main(void) {
 	for (int i = 0; i < 2; i++) {
 		gpio_act_on();
-		volatile size_t w = 0xC0000;
-		while (w--) ;
+		wait(BLINK_WAIT);
 
 		gpio_act_off();
-		w = 0xC0000;
-		while (w--) ;
+		wait(BLINK_WAIT);
 	}
 
 	while (1) asm volatile ("wfe");
