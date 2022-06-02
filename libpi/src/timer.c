92cfe6f73a8b91ffa68584e299350320bada22c8
Justin Weiler <jweiler@stanford.edu>
2022-06-02 15:18:50 -0700

    add LAST READ comments

--- libpi/src/timer.c
+++ libpi/src/timer.c
@@ -4,6 +4,7 @@
 
 void delay_usec(uint64_t usec) {
 	uint64_t now = timer_get_usec_raw();
+	// LAST READ
 	while (timer_get_usec_raw() - now < usec) ;
 	MB_RD;
 }
@@ -18,6 +19,7 @@ void delay_sec(uint64_t sec) {
 
 
 uint64_t timer_get_usec(void) {
+	// LAST READ
 	uint64_t ret = timer_get_usec_raw();
 	MB_RD;
 	return ret;
