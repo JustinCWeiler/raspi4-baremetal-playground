270c8e9e283113f90102803f02e1ca674e210277
Justin Weiler <jweiler@stanford.edu>
2022-06-05 19:41:23 -0700

    move gpio functions to gpio.c

---
+++ bootloader-c/src/gpio.c
@@ -0,0 +1,13 @@
+#define GPIO_BASE (PERIPHERAL_BASE+0x00200000)
+#define GPIO_SET1 ((volatile uint32_t*)(GPIO_BASE+0x20))
+#define GPIO_CLR1 ((volatile uint32_t*)(GPIO_BASE+0x2C))
+
+static void gpio_act_on(void) {
+	// WRITE
+	*GPIO_SET1 = 1 << 10;
+}
+
+static void gpio_act_off(void) {
+	// WRITE
+	*GPIO_CLR1 = 1 << 10;
+}
--- bootloader-c/src/main.c
+++ bootloader-c/src/main.c
@@ -2,27 +2,16 @@
 #include <stddef.h>
 
 #define PERIPHERAL_BASE 0xFE000000
-#define GPIO_BASE (PERIPHERAL_BASE+0x00200000)
-#define GPIO_SET1 ((volatile uint32_t*)(GPIO_BASE+0x20))
-#define GPIO_CLR1 ((volatile uint32_t*)(GPIO_BASE+0x2C))
+
+#include "gpio.c"
 
 #define BLINK_WAIT 0xC0000
-
-static void gpio_act_on(void) {
-	// WRITE
-	*GPIO_SET1 = 1 << 10;
-}
-
-static void gpio_act_off(void) {
-	// WRITE
-	*GPIO_CLR1 = 1 << 10;
-}
 
 static void wait(volatile size_t w) {
 	while (w--) ;
 }
 
-__attribute__((flatten, noreturn, nothrow))
+__attribute__((flatten, noreturn, nothrow, cold))
 void main(void) {
 	for (int i = 0; i < 2; i++) {
 		gpio_act_on();
