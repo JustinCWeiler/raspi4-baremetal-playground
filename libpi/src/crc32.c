3f5b9db05a2e8163f9a9ceeaecf66cb2da8e4d82
Justin Weiler <jweiler@stanford.edu>
2022-06-06 18:40:41 -0700

    change =& to +

--- libpi/src/crc32.c
+++ libpi/src/crc32.c
@@ -7,7 +7,7 @@ uint32_t crc32(const void* void_buf, size_t n) {
 	uint32_t crc = ~0;
 	for (size_t i = 0; i < n; i++) {
 		uint8_t val = buf[i];
-		asm volatile ("crc32b %w0, %w0, %w1" : "=&r" (crc) : "r" (val));
+		asm volatile ("crc32b %w0, %w0, %w1" : "+r" (crc) : "r" (val));
 	}
 	return ~crc;
 }
